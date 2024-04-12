5e1a4568cc13f37172e8a589df84445e5178eeb1
hjh <1138440664@qq.com>
2024-04-12 08:47:40 +0800

    hi 111

--- src/main/java/vsl/demo1.java
+++ src/main/java/vsl/demo1.java
@@ -3,5 +3,6 @@ package vsl;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("sssssssssssssssssssssssssssssssssssssssssss");
+        System.out.println("hhjhggjgjghgjhgjgjgjjgjggj");
     }
 }
